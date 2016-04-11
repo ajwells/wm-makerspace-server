2b68310e651e52e50fb775c9a68b6e2bfeff798b
ajwells <ajwells@email.wm.edu>
2016-04-10 20:30:44 -0400

    setup

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var cors = require('cors');
+var pg = require('pg');
+
+var app = express();
+app.use(cors());
+var PORT = 12000;
+
+var conString = "postgres://ajwells:ajwells@localhost/ajwells_company";
+
+app.get('/test', function(req, res) {
+	fetch(test)
+		.then(function(url) { req.send(url); })
+		.catch(function(err) { res.status(500).send(err); });
+});
+
+app.listen(PORT, function() {
+	console.log('Server listening on port:', PORT);
+});
+
+function fetch(type) {
+	return new Promise(function(resolve, reject) {
+		pg.connect(conString, function(err, client, done) {
+			if (err) return reject(err);
+			switch (type) {
+				case 'test':
+					var query = 'select * from employee';
+			}
+			client.query(query, function(error, result) {
+				done();
+				if (error) return reject(error);
+				resolve(result);
+			});
+		});
+	});
+}
+
