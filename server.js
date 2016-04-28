fec4a2de502b4a63b9e14e98c8061ea52c57660c
ajwells <ajwells@email.wm.edu>
2016-04-28 10:47:35 +0000

    final server?

--- server.js
+++ server.js
@@ -18,17 +18,21 @@ app.get('/day', function(req, res) {
 		.catch(function(err) { res.status(500).send(err); });
 });
 
+app.get('/member/time/:id', function(req, res) {
+	var id = req.params.id;
+	var query = "select extract(hour from time_in) as time_in, extract(hour from time_out) as time_out \
+			from visited natural join sessions \
+			where member_id = " + id + ";";
+	fetch(query)
+		.then(function(url) { res.send(url); })
+		.catch(function(err) { res.status(500).send(err); });
+});
+
 app.get('/time/:type', function(req, res) {
 	var type = req.params.type;
 	if (type == 'all') {
 		var query = "select extract(hour from time_in) as time_in, extract(hour from time_out) as time_out \
-				from visited natural join sessions \
-				where member_id IN ( \
-					(select member_id as id \
-					from skilled_in) \
-					union \
-					(select member_id as id \
-					from certified_in));";
+				from visited natural join sessions;";
 	} else {
 		var query = "select extract(hour from time_in) as time_in, extract(hour from time_out) as time_out \
 				from visited natural join sessions \
@@ -36,10 +40,10 @@ app.get('/time/:type', function(req, res) {
 					(select member_id as id \
 					from skilled_in \
 					where skill = \'"+ type +"\') \
-				union \
-				(select member_id as id \
-				from certified_in \
-				where certificate = \'"+ type +"\'));";
+					union \
+					(select member_id as id \
+					from certified_in \
+					where certificate = \'"+ type +"\'));";
 	}
 	fetch(query)
 		.then(function(url) { res.send(url); })
@@ -47,8 +51,8 @@ app.get('/time/:type', function(req, res) {
 });
 
 app.get('/interestlist', function(req, res) {
-	var query = 'select distinct interest \
-			from interested_in;';
+	var query = 'select distinct skill as interest \
+			from skilled_in;';
 	fetch(query)
 		.then(function(url) { res.send(url); })
 		.catch(function(err) { res.status(500).send(err); });
@@ -61,17 +65,17 @@ app.get('/makerspace/:type', function(req, res) {
 		case 'current':
 			query = 'select member_id as id, name, max(time_in) as last_visit \
 				from member natural join (visited natural join sessions) \
-				where time_out = NULL \
+				where time_out is NULL \
 				group by member_id;';
 			break;
 		case 'notcurrent':
-			query = '(select member_id as id, name, max(time_in) as last_visit \
-					from member natural join (visited natural join sessions) \
-					group by member_id) \
+			query = '(select a.member_id as id, name, max(time_in) as last_visit \
+				from member as a left outer join (visited natural join sessions) as b on (a.member_id = b.member_id) \
+				group by a.member_id) \
 				except \
 				(select member_id as id, name, max(time_in) as last_visit \
 					from member natural join (visited natural join sessions) \
-					where time_out = NULL \
+					where time_out is NULL \
 					group by member_id);';
 			break;
 	}
@@ -81,9 +85,9 @@ app.get('/makerspace/:type', function(req, res) {
 });
 
 app.get('/memberlist', function(req, res) {
-	var query = 'select member_id as id, name, count(session_id) as visit, max(time_in) as last_visit \
-			from member natural join (visited natural join sessions) \
-			group by member_id;';
+	var query = 'select a.member_id as id, name, count(session_id) as visit, max(time_in) as last_visit \
+			from member as a left outer join (visited natural join sessions) as b on (a.member_id = b.member_id) \
+			group by a.member_id;';
 	fetch(query)
 		.then(function(url) { res.send(url); })
 		.catch(function(err) { res.status(500).send(err); });
@@ -182,9 +186,82 @@ app.get('/images/:name', function(req, res) {
 app.post('/new/:type', function(req, res) {
 	var type = req.params.type;
 	var data = req.body;
+	var query;
+	switch(type) {
+		case 'signin':
+			query = 'insert into sessions(location) values(\'' + req.body.room + '\'); \
+				insert into visited(member_id, session_id) values(' + req.body.id + ', currval(\'session_seq\'));';
+			break;
+		case 'signout':
+			query = 'update sessions set time_out = CURRENT_TIMESTAMP \
+				where session_id = ( \
+					select session_id \
+					from visited natural join sessions \
+					where member_id = ' + req.body.id + ' and time_out is NULL);';
+			break;
+		case 'member':
+			query = 'insert into member(member_id, name) values(' + req.body.id + ', \'' + req.body.name + '\');';
+			req.body.interests.forEach(function(data, index) {
+				query += 'insert into interested_in(member_id, interest) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			req.body.skills.forEach(function(data, index) {
+				query += 'insert into skilled_in(member_id, skill) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			req.body.certs.forEach(function(data, index) {
+				query += 'insert into certified_in(member_id, certificate) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			break;
+		case 'project':
+			query = 'insert into projects(project_name, budget) values(\'' + req.body.name + '\', ' + req.body.budget + ');';
+			req.body.members.forEach(function(data, index) {
+				query += 'insert into works_on(member_id, project_id) values(' + data + ', currval(\'project_seq\'));';
+			}.bind(this));
+			break;
+	}
 	console.log(type);
-	console.log(req.body);
-	res.send('ok');
+	console.log(req.body.id);
+	fetch(query)
+		.then(function() { res.send('ok'); })
+		.catch(function(err) { res.status(500).send(err); });
+});
+
+app.post('/update/:type', function(req, res) {
+	var type = req.params.type;
+	var data = req.body;
+	var query = '';
+	switch(type) {
+		case 'member':
+			if (req.body.name != '') {
+				query += 'update member set name = \'' + req.body.name + '\' \
+						where member_id = ' + req.body.id + ';';
+			}
+			req.body.interests.forEach(function(data, index) {
+				query += 'insert into interested_in(member_id, interest) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			req.body.skills.forEach(function(data, index) {
+				query += 'insert into skilled_in(member_id, skill) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			req.body.certs.forEach(function(data, index) {
+				query += 'insert into certified_in(member_id, certificate) values(' + req.body.id + ', \'' + data + '\');';
+			}.bind(this));
+			break;
+		case 'project':
+			if (req.body.budget != '') {
+				query += 'update projects set budget = \'' + req.body.budget + '\' \
+						where project_id = ' + req.body.id + ';';
+			}
+			if (req.body.spent!= '') {
+				query += 'update projects set spent = \'' + req.body.spent + '\' \
+						where project_id = ' + req.body.id + ';';
+			}
+			req.body.members.forEach(function(data, index) {
+				query += 'insert into works_on(member_id, project_id) values(' + data + ', ' + req.body.id + ');';
+			}.bind(this));
+			break;
+	}
+	fetch(query)
+		.then(function() { res.send('ok'); })
+		.catch(function(err) { res.status(500).send(err); });
 });
 
 app.delete('/delete/:type/:id/:item', function(req, res) {
@@ -205,6 +282,10 @@ app.delete('/delete/:type/:id/:item', function(req, res) {
 			query = 'delete from certified_in \
 				where member_id = ' + id + ' and certificate = \'' + item + '\';';
 			break;
+		case 'project':
+			query = 'delete from works_on \
+				where member_id = ' + id + ' and project_id = \'' + item + '\';';
+			break;
 	}
 	fetch(query)
 		.then(function() { res.send('ok'); })
@@ -222,7 +303,7 @@ function fetch(query) {
 
 			client.query(query, function(error, result) {
 				done();
-				if (error) return reject(error);
+				if (error) {console.log(error);return reject(error);}
 				resolve(result.rows);
 			});
 		});
